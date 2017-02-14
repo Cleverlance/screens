2dddbce8ba94c135eb700f5d53a49ef86b05b95f
Vojtech Kaisler <vojtech.kaisler@cleverlance.com>
2017-02-14 09:30:09 +0100

    Change Screen invoker property injection into protected abstract property

--- screens/src/main/kotlin/com/cleverlance/mobile/android/screens/domain/ScreenInvoker.kt
+++ screens/src/main/kotlin/com/cleverlance/mobile/android/screens/domain/ScreenInvoker.kt
@@ -2,10 +2,9 @@ package com.cleverlance.mobile.android.screens.domain
 
 import android.app.Activity
 import com.cleverlance.mobile.android.screens.presenter.ScreenPresenter
-import javax.inject.Inject
 
 abstract class ScreenInvoker {
-    @Inject internal lateinit var screenPresenter: ScreenPresenter
+    protected abstract val screenPresenter: ScreenPresenter
     protected abstract val screenFactory: ScreenFactory
 
     // TODO rename showScreen()
--- screens/src/test/kotlin/com/cleverlance/mobile/android/screens/domain/ScreenInvokerSpec.kt
+++ screens/src/test/kotlin/com/cleverlance/mobile/android/screens/domain/ScreenInvokerSpec.kt
@@ -1,6 +1,7 @@
 package com.cleverlance.mobile.android.screens.domain
 
 import android.app.Activity
+import com.cleverlance.mobile.android.screens.presenter.ScreenPresenter
 import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.equalTo
 import com.nhaarman.mockito_kotlin.*
@@ -12,35 +13,40 @@ import org.junit.runner.RunWith
 
 @RunWith(JUnitPlatform::class)
 internal class ScreenInvokerSpec : SubjectSpek<ScreenInvoker>({
-    subject {
-        object : ScreenInvoker() {
-            override val screenFactory: ScreenFactory
-                get() = object : ScreenFactory {
-                    override fun createScreen(back: ((Activity) -> Boolean)): Screen {
-                        return BaseScreen(back = back,
-                                viewProvider = mock <ViewProvider <*>>())
+    var screenPresenter = spy<ScreenPresenter>()
+    beforeEachTest {
+        screenPresenter = spy<ScreenPresenter>()
+        subject {
+            object : ScreenInvoker() {
+                override val screenPresenter: ScreenPresenter
+                    get() = screenPresenter
+                override val screenFactory: ScreenFactory
+                    get() = object : ScreenFactory {
+                        override fun createScreen(back: ((Activity) -> Boolean)): Screen {
+                            return BaseScreen(back = back,
+                                    viewProvider = mock <ViewProvider <*>>())
+                        }
                     }
-                }
-        }.apply {
-            screenPresenter = spy()
+            }
         }
+
     }
 
     it("should create screen") {
         subject.createScreen()
 
-        verify(subject.screenPresenter).setScreen(any<BaseScreen<*>>())
+        verify(screenPresenter).setScreen(any<BaseScreen<*>>())
     }
 
     it("should create screen with reset") {
         subject.createScreen({ false })
 
         argumentCaptor<Screen>().run {
-            verify(subject.screenPresenter).setScreen(capture())
+            verify(screenPresenter).setScreen(capture())
 
             assertThat(allValues.size, equalTo(1))
             println(lastValue)
-            assertThat(subject.screenPresenter.back(mock()), equalTo(false))
+            assertThat(screenPresenter.back(mock()), equalTo(false))
         }
     }
 
@@ -48,13 +54,13 @@ internal class ScreenInvokerSpec : SubjectSpek<ScreenInvoker>({
         it("should default back to previous screen") {
             val firstScreen = mock<Screen>()
 
-            subject.screenPresenter.setScreen(firstScreen)
+            screenPresenter.setScreen(firstScreen)
 
             subject.createScreen()
 
-            assertThat(subject.screenPresenter.back(mock()), equalTo(true)) // back consumed
+            assertThat(screenPresenter.back(mock()), equalTo(true)) // back consumed
 
-            assertThat(subject.screenPresenter.getScreen(), equalTo(firstScreen))
+            assertThat(screenPresenter.getScreen(), equalTo(firstScreen))
         }
     }
 })
