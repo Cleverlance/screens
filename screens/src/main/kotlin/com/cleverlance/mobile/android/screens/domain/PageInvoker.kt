07cc377a39ae5f78a86645a73fef9360ddc6a722
Vojtech Kaisler <vojtech.kaisler@cleverlance.com>
2017-07-20 10:24:45 +0200

    Remove class injection from screens

--- screens/src/main/kotlin/com/cleverlance/mobile/android/screens/domain/PageInvoker.kt
+++ screens/src/main/kotlin/com/cleverlance/mobile/android/screens/domain/PageInvoker.kt
@@ -1,10 +1,9 @@
 package com.cleverlance.mobile.android.screens.domain
 
 import com.cleverlance.mobile.android.screens.presenter.BasePresenterView
-import javax.inject.Inject
 
 abstract class PageInvoker<V : BasePresenterView> {
-    @Inject internal lateinit var viewProvider: ViewProvider<V>
+    abstract var viewProvider: ViewProvider<V>
 
     fun createPage() = PageScreen(viewProvider)
 }
