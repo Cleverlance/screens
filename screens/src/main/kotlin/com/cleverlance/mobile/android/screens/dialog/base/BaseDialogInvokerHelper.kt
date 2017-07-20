a6b6f650573c28a73edf5aa341ba4dc864f830f6
Vojtech Kaisler <vojtech.kaisler@cleverlance.com>
2017-07-20 10:37:47 +0200

    Add base dialog template

---
+++ screens/src/main/kotlin/com/cleverlance/mobile/android/screens/dialog/base/BaseDialogInvokerHelper.kt
@@ -0,0 +1,6 @@
+package com.cleverlance.mobile.android.screens.dialog.base
+
+import com.cleverlance.mobile.android.screens.dialog.GenericDialogInvokeHelper
+import com.cleverlance.mobile.android.screens.dialog.android.DialogScreen
+
+abstract class BaseDialogInvokerHelper : GenericDialogInvokeHelper<DialogScreen>()
