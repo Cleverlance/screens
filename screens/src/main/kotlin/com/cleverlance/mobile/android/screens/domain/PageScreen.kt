c6dd12a5843b2ae4d2f0628d9edc31afc623557e
Tomas Kotula <tomas.kotula@cleverlance.com>
2017-07-20 16:25:03 +0200

    Refactor pager adapter

--- screens/src/main/kotlin/com/cleverlance/mobile/android/screens/domain/PageScreen.kt
+++
@@ -1,6 +0,0 @@
-package com.cleverlance.mobile.android.screens.domain
-
-class PageScreen(
-        private val viewFactory: ViewFactory) : Screen() {
-    override fun createView() = viewFactory.createView()
-}
--- screens/src/main/kotlin/com/cleverlance/mobile/android/screens/view/ScreenPagerAdapter.kt
+++ screens/src/main/kotlin/com/cleverlance/mobile/android/screens/view/ScreenPagerAdapter.kt
@@ -3,26 +3,25 @@ package com.cleverlance.mobile.android.screens.view
 import android.support.v4.view.PagerAdapter
 import android.view.View
 import android.view.ViewGroup
-import com.cleverlance.mobile.android.screens.domain.PagerConfiguration
 import com.cleverlance.mobile.android.screens.domain.Screen
 import org.jetbrains.anko.AnkoComponent
 import org.jetbrains.anko.AnkoContext
 import org.jetbrains.anko.matchParent
 import org.jetbrains.anko.verticalLayout
 
-open class BasePagerAdapter(val pagerConfig: PagerConfiguration) : PagerAdapter() {
+abstract class ScreenPagerAdapter : PagerAdapter() {
 
-    override fun instantiateItem(container: ViewGroup?, position: Int) =
-            createScreenContainer(container!!, pagerConfig.getTab(position))
+    override fun instantiateItem(container: ViewGroup, position: Int) =
+            createScreenContainer(container, getScreen(position))
                     .apply { container.addView(this) }
+
+    abstract fun getScreen(position: Int): Screen
 
     override fun destroyItem(container: ViewGroup, position: Int, view: Any) {
         container.removeView(view as View)
     }
 
-    override fun isViewFromObject(view: View?, any: Any?) = view == any
-
-    override fun getCount() = pagerConfig.count()
+    override fun isViewFromObject(view: View, any: Any) = view == any
 
     private fun createScreenContainer(container: ViewGroup, screen: Screen) =
             object : AnkoComponent<ViewGroup> {
