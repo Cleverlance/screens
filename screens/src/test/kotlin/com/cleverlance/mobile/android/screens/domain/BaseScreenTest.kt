e7342e138b5e35746a0cc7896a59ae10c37f4cff
Tomas Kotula <tomas.kotula@cleverlance.com>
2017-07-24 17:25:50 +0200

    Fix test

--- screens/src/test/kotlin/com/cleverlance/mobile/android/screens/domain/BaseScreenTest.kt
+++ screens/src/test/kotlin/com/cleverlance/mobile/android/screens/domain/BaseScreenTest.kt
@@ -2,10 +2,8 @@ package com.cleverlance.mobile.android.screens.domain
 
 import com.cleverlance.mobile.android.screens.presenter.BasePresenterView
 import com.natpryce.hamkrest.assertion.assertThat
-import com.natpryce.hamkrest.isA
+import com.natpryce.hamkrest.equalTo
 import com.nhaarman.mockito_kotlin.mock
-import com.nhaarman.mockito_kotlin.spy
-import com.nhaarman.mockito_kotlin.whenever
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.context
 import org.jetbrains.spek.api.dsl.it
@@ -16,16 +14,14 @@ import org.junit.runner.RunWith
 internal class BaseScreenSpec : Spek({
     context("create view") {
         it("should create view") {
-            val viewProvider = spy<ViewProvider<BasePresenterView>>()
             val createdView = mock<BasePresenterView>()
-            viewProvider.viewProvider = mock { whenever(it.get()).thenReturn(createdView) }
             val screen = object : BaseScreen() {
                 override fun createView(): BasePresenterView {
                     return createdView
                 }
             }
 
-            assertThat(screen.createView(), isA<BasePresenterView>())
+            assertThat(screen.createView(), equalTo(createdView))
         }
     }
 })
