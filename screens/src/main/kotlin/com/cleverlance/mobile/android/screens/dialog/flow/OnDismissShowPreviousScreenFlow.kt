2dd8a0e7255cea56e0974c52b0f92348e9ec637f
Tomas Kotula <tomas.kotula@cleverlance.com>
2017-12-12 19:04:56 +0100

    Simplify ScreenPresenter

--- screens/src/main/kotlin/com/cleverlance/mobile/android/screens/dialog/flow/OnDismissShowPreviousScreenFlow.kt
+++ screens/src/main/kotlin/com/cleverlance/mobile/android/screens/dialog/flow/OnDismissShowPreviousScreenFlow.kt
@@ -4,11 +4,17 @@ import com.cleverlance.mobile.android.screens.dialog.ScreenFlow
 import com.cleverlance.mobile.android.screens.domain.BaseScreen
 import com.cleverlance.mobile.android.screens.presenter.ScreenPresenter
 import io.reactivex.disposables.Disposable
+import io.reactivex.disposables.Disposables
 
 class OnDismissShowPreviousScreenFlow(private val screenPresenter: ScreenPresenter) : ScreenFlow<BaseScreen> {
     override fun show(screen: BaseScreen): Disposable =
             screenPresenter.onDisposeShowCurrent().apply {
                 screen.onShow(this)
-                screenPresenter.setScreen(screen)
+                screenPresenter.screen = screen
             }
+
+    private fun ScreenPresenter.onDisposeShowCurrent(): Disposable {
+        val previousScreen = screen // capture current screen
+        return Disposables.fromRunnable { screen = previousScreen }
+    }
 }
