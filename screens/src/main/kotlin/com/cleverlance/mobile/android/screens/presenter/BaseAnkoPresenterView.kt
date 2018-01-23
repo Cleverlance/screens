13eca3f31b7c4ed6874814de27c666b5b05fef9e
Tomas Kotula <tomas.kotula@cleverlance.com>
2018-01-23 12:47:00 +0100

    Enable AnkoComponent previews

--- screens/src/main/kotlin/com/cleverlance/mobile/android/screens/presenter/BaseAnkoPresenterView.kt
+++ screens/src/main/kotlin/com/cleverlance/mobile/android/screens/presenter/BaseAnkoPresenterView.kt
@@ -2,11 +2,14 @@ package com.cleverlance.mobile.android.screens.presenter
 
 import android.view.View
 import android.view.ViewGroup
+import org.jetbrains.anko.AnkoComponent
 import org.jetbrains.anko.AnkoContext
 
-abstract class BaseAnkoPresenterView : BasePresenterView() {
+abstract class BaseAnkoPresenterView : BasePresenterView(), AnkoComponent<ViewGroup> {
     override fun createView(container: ViewGroup) =
-            createAnkoView(AnkoContext.create(container.context, container))
+            createView(AnkoContext.create(container.context, container))
+
+    override fun createView(ui: AnkoContext<ViewGroup>): View = createAnkoView(ui)
 
     abstract fun createAnkoView(ui: AnkoContext<ViewGroup>): View
 }
